24f65a82ad324839908bee6291355603150b005e
ranabilal-hub <ranabilaltahir7@gmail.com>
2026-09-05 22:03:59 +0500

    Add task 6 program to check if a number is even or odd

---
+++ home-lab03/task6_evenodd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+int num ;
+printf("Type your num :");
+scanf("%d",&num);
+
+if (num % 2 == 0){  
+printf("%d is Even", num);
+} else {
+    printf("%d is Odd", num);
+}
+return 0;
+}

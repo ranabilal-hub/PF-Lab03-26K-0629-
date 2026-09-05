2d52525386cfcfe9224cf1d4c996900b677185fb
ranabilal-hub <ranabilaltahir7@gmail.com>
2026-09-05 21:59:20 +0500

    Add task2_variable.c

---
+++ home-lab03/task2_variable.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+char Name[] = "Rana Bilal";
+int age = 18;
+float cgpa = 3.74;
+char grade = 'A';
+long rollno = 165243908;
+
+printf("Name : %s | age : %d | grade : %c | cgpa : %f | rollno: %ld\n", Name , age , grade , cgpa , rollno );
+
+return 0;
+
+}

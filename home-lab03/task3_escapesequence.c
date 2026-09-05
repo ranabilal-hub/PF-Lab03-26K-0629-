d37277253874926ca9c5181916f08f077bcbce9c
ranabilal-hub <ranabilaltahir7@gmail.com>
2026-09-05 22:00:21 +0500

    Add task3_escapesequence.c
    This program prints user information and a quote, including an alert sound.

---
+++ home-lab03/task3_escapesequence.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+printf("=====================\n");
+printf("Name:\t\tRana Bilal\n");
+printf("rollnumber:\t10245637\n");
+printf(":Department:\tComputer science\n");
+printf("Quote:\t\t\"Hard work never goes in vain\"\n");
+printf("Path :\t\tC:\\User\\Rana Bilal\n");
+printf("Alert\a\n");
+printf("=====================\n");
+
+return 0;
+}
